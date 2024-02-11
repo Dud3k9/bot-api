4884846a1c510bbaf93cd8f0d33372a4719b9f97
Dud3k9 <petrykowski99@gmail.com>
2024-02-11 19:46:54 +0100

    deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @Cron('45 * * * * *')
+  handleCron() {
+    console.log('cron-test');
+    this.logger.debug('cron-test');
   }
 }
