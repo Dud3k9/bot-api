ef09632092c6ee4f394b83465a038cc1403a9cd2
Maciej Petrykowski <petrykowski99@gmail.com>
2024-02-12 21:12:18 +0100

    bot init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
-  @Get('test')
+  @Get("test")
   getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get("start-bot")
+  startBot(): string {
+    this.appService.startService();
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,23 +1,46 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
-
+import { Injectable, Logger } from "@nestjs/common";
+import * as playwright from "playwright";
 @Injectable()
 export class AppService {
   private readonly logger = new Logger(AppService.name);
 
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
   }
+  async startService(){
+    const browser = await playwright['chromium'].launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage(); //wait for the browser to create a new page by putting await
+    await page.goto("https://share.parkanizer.com/refresh-session"); //opening Google page
+    await page.waitForLoadState("load");
+    await page.waitForTimeout(2000);
+    const emailInput = await page.$("[id='signInName']");
+    await emailInput.fill('extern.petrykowski_maciej@mondial-assistance.pl')
+    const submitButton = await page.$("[id='continue']");
+    await submitButton.click();
+    await page.waitForTimeout(2000);
+    const passwordInput = await page.$("[id='password']");
+    await passwordInput.fill("Syryjski1");
+    await page.waitForTimeout(2000);
+    const nextButton = await page.$("[id='next']");
+    await nextButton.click();
+    await page.waitForTimeout(2000);
+    const cookieButton = await page.$("[id='confirm-cookie-consent']");
+    await cookieButton.click();
+    const startTidaroButton = await page.getByText("Start using Tidaro");
+    await page.waitForTimeout(1000);
+    await startTidaroButton.click();
+    await page.waitForTimeout(1000);
+    const dialogOk = await page.$("[id='dialog-ok']");
+    await dialogOk.click();
+    await page.waitForTimeout(1000);
+    await page.goto("https://share.parkanizer.com/marketplace");
 
-  // test() {
-  //   setInterval(() => {
-  //     console.log('test interval');
-  //   }, 2000);
-  // }
 
-  @Cron('45 * * * * *')
-  handleCron() {
-    console.log('cron-test');
-    this.logger.debug('cron-test');
   }
 }
+
+
+            
+
+        
